84e37e1cac8ea36de3a9c280b32660a98e275dcb
AchiraLucy <achiralucy@gmail.com>
2026-03-10 23:39:03 +0700

    Merge project Pertemuan 3 dari repo terpisah ke repo utama

--- app/src/main/java/com/example/praktam_2417051069/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051069/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.praktam_2417051069.model.StudentSource
 import com.example.praktam_2417051069.ui.theme.PrakTAM_2417051069Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,17 +36,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, npm : String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Halo $name saya dengan $npm siap belajar compose",
+fun Greeting(name: String, npm: String, modifier: Modifier = Modifier) {
+
+    val student = StudentSource.dummystudentplanner[0]
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+
+        Text(text = "Halo $name saya dengan $npm siap belajar compose")
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Nama: ${student.nama}")
+        Text(text = "Deadline: ${student.deadline}")
+        Text(text = "Status: ${student.status}")
+
+        Image(
+            painter = painterResource(id = student.gambar),
+            contentDescription = student.nama,
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PrakTAM_2417051069Theme {
-        Greeting(name = "Android", npm = "2417051069")
+        Greeting(name = "Achira Desya Lucy", npm = "2417051069")
     }
 }
---
+++ app/src/main/java/com/example/praktam_2417051069/model/studentsource.kt
@@ -0,0 +1,13 @@
+package com.example.praktam_2417051069.model
+
+import com.example.praktam_2417051069.R
+
+object StudentSource {
+
+    val dummystudentplanner = listOf(
+        StudentPlanner("Libur", "01/10/2025", "No Activity", R.drawable.okt),
+        StudentPlanner("Rapat Hima", "20/12/2024 di Teman Nongki", "Not Yet", R.drawable.tanggal),
+        StudentPlanner("Tugas RPL", "15/02/2025", "Done", R.drawable.tmerah)
+    )
+
+}
