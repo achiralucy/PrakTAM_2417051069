7ed083bf98c998416ee3f1dc8a9c237a53279524
AchiraLucy <achiralucy@gmail.com>
2026-03-11 13:07:28 +0700

    Tugas LKP 3

--- app/src/main/java/com/example/praktam_2417051069/model/studentsource.kt
+++ app/src/main/java/com/example/praktam_2417051069/model/studentsource.kt
@@ -5,9 +5,10 @@ import com.example.praktam_2417051069.R
 object StudentSource {
 
     val dummystudentplanner = listOf(
-        StudentPlanner("Libur", "01/10/2025", "No Activity", R.drawable.okt),
-        StudentPlanner("Rapat Hima", "20/12/2024 di Teman Nongki", "Not Yet", R.drawable.tanggal),
-        StudentPlanner("Tugas RPL", "15/02/2025", "Done", R.drawable.tmerah)
+        StudentPlanner("Tugas MTK", "15/02/2025", "Halaman 30 No 1-5, Soal gausah ditulis", R.drawable.math),
+        StudentPlanner("Libur", "01/10/2025", "Waktunya bengong", R.drawable.okt),
+        StudentPlanner("Rapat Hima", "20/12/2026 di Teman Nongki", "JANGAN TELAT", R.drawable.tanggal)
+
     )
 
 }
